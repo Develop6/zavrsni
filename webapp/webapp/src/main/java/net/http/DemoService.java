d3f637811b308f49904865a0d2891b76a0ffdd34
Develop6 <ivan.daska@gmail.com>
2014-08-24 12:00:15 +0200

    Changing task: DemoService

--- webapp/webapp/src/main/java/net/http/DemoService.java
+++ webapp/webapp/src/main/java/net/http/DemoService.java
@@ -3,10 +3,12 @@ package net.http;
 import java.util.Date;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DemoService {
 
-    @Scheduled(cron="*/5 * * * * ?")
+	@Scheduled(fixedRate = 3000)
     public void demoServiceMethod()
     {
         System.out.println("Method executed at every 5 seconds. Current time is :: "+ new Date());
